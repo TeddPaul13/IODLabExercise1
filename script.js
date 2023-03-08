a388d37cf06c31283a4d743f4e850052e0c618a1
TeddPaul13 <teddtycoon@gmail.com>
2023-03-08 22:51:43 +1100

    Updated UI and roll button functionality

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 
-//document.getElementById("oneDice").addEventListener("click", roll4SideDice)
-function rollDice1(min, max){
+
+
+//This function rolls dice if only one dice selected
+function rollDice1(min, max) {
     document.getElementById("diceNumber1").innerHTML = 
     Math.floor(Math.random() * (max-min)+min);
 }
+
+//This function rolls Dice if two dice selected
 function rollAllDice(min, max){
 
     document.getElementById("diceNumber1").innerHTML = 
@@ -13,13 +17,14 @@ function rollAllDice(min, max){
     Math.floor(Math.random() * (max-min)+min);
 }
 
-
+// This function select and displays the number of dice to be rolled
 function displayDice(){
     let x = document.getElementById("my-selector1").value;
 
     if (x === "1"){
         document.getElementById("second-dice").style.display = "none";
         document.getElementById("twoDice").style.display = "none";
+        document.getElementById("oneDice").style.display = "flex";
     }
     if (x === "2"){
         document.getElementById("second-dice").style.display ="flex";
@@ -27,4 +32,36 @@ function displayDice(){
         document.getElementById("oneDice").style.display = "none";
     }
 
+}
+//This function (selects and updates the number of sides on the dice) and enables buttons
+function selectDiceSides(){
+    let firstDropdown = document.getElementById("side-selector").value;
+    let secondDropdown = document.getElementById("my-selector1").value;
+    
+    if (firstDropdown == 4){
+        document.getElementById("diceNumber1").innerHTML = firstDropdown;
+        document.getElementById("diceNumber2").innerHTML = firstDropdown;
+    }
+    if (firstDropdown == 6){
+        document.getElementById("diceNumber1").innerHTML = firstDropdown;
+        document.getElementById("diceNumber2").innerHTML = firstDropdown;
+    }
+    if (firstDropdown == 10){
+        document.getElementById("diceNumber1").innerHTML = firstDropdown;
+        document.getElementById("diceNumber2").innerHTML = firstDropdown;
+    }
+    
+    if(firstDropdown !== "0" && secondDropdown !== "0"){
+       
+        document.getElementById("oneDice").disabled = false;
+       document.getElementById("twoDice").disabled = false;
+    }else{
+        document.getElementById("oneDice").disabled = true;
+       document.getElementById("twoDice").disabled = true;
+    }
+}
+
+function testFunction(){
+    displayDice();
+    selectDiceSides();
 }
